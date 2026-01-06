e78fb0852549fa375efe0c7498987667d2df6e9c
SaifHaiderKayani <saifhaiderkayani@gmail.com>
2026-01-06 21:14:14 +0800

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+  document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+    alert('Right-click is disabled for this site to protect the content.');
+  });
+</script>
