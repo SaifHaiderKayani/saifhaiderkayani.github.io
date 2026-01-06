1d8488a7b7121471a5355fa6abbe9d88786d3248
SaifHaiderKayani <saifhaiderkayani@gmail.com>
2026-01-06 21:42:23 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-<script type="text/javascript">
-  document.addEventListener('contextmenu', function(e) {
+document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
     alert('Right-click is disabled for this site to protect the content.');
-  });
-</script>
+});
